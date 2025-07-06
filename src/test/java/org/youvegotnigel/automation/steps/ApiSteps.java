31a0f86c0733c2ed8d62ed7c6ccc96320e0038eb
Nigel <nigeldave19@gmail.com>
2025-07-06 18:19:33 +0530

    Update ApiSteps.java

--- src/test/java/org/youvegotnigel/automation/steps/ApiSteps.java
+++ src/test/java/org/youvegotnigel/automation/steps/ApiSteps.java
@@ -8,6 +8,7 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.youvegotnigel.automation.context.ApiTestContext;
 import org.youvegotnigel.automation.pojo.CreateOrder;
 import org.youvegotnigel.automation.pojo.LoginRequest;
@@ -49,7 +50,7 @@ public class ApiSteps {
 
 
     @And("^I add a watch product with below information:$")
-    public void add_product(DataTable dataTable) {
+    public void add_a_watch_product(DataTable dataTable) {
 
         RequestSpecification addProductReqSpec = new RequestSpecBuilder().setBaseUri(Routes.BASE_URL)
                 .addHeader("Authorization", context.getToken()).build();
@@ -61,6 +62,24 @@ public class ApiSteps {
                         .formParams(formValues)
                         .param("productAddedBy", context.getUserId())
                         .multiPart("productImage", new File(Routes.WATCH_IMAGE_PATH));
+
+        String addProductResponse = reqAddProduct.when().post(Routes.ADD_PRODUCT_PATH).then().log().all().extract().response().asString();
+        context.setProductId(new JsonPath(addProductResponse).getString("productId"));
+    }
+
+    @And("^I add a shoe product with below information:$")
+    public void add_a_shoe_product(DataTable dataTable) {
+
+        RequestSpecification addProductReqSpec = new RequestSpecBuilder().setBaseUri(Routes.BASE_URL)
+                .addHeader("Authorization", context.getToken()).build();
+
+        Map<String, String> formValues = dataTable.transpose().asMap();
+
+        RequestSpecification reqAddProduct =
+                given().spec(addProductReqSpec).log().all()
+                        .formParams(formValues)
+                        .param("productAddedBy", context.getUserId())
+                        .multiPart("productImage", new File(Routes.SHOE_IMAGE_PATH));
 
         String addProductResponse = reqAddProduct.when().post(Routes.ADD_PRODUCT_PATH).then().log().all().extract().response().asString();
         context.setProductId(new JsonPath(addProductResponse).getString("productId"));
@@ -94,19 +113,60 @@ public class ApiSteps {
         context.setOrderId(new JsonPath(resCreateOrder).getString("orders[0]"));
     }
 
-    
+
     @Then("^the order details should be displayed as below:$")
     public void verify_order_details(DataTable dataTable) {
 
         RequestSpecification getOrderDetailsReqSpec = new RequestSpecBuilder().setBaseUri(Routes.BASE_URL)
                 .addHeader("Authorization", context.getToken()).build();
 
-        // NOTE: Use case of query parameters
         RequestSpecification reqGetOrder =
                 given().spec(getOrderDetailsReqSpec).log().all().param("id", context.getOrderId());
 
-        reqGetOrder.when().get(Routes.ORDER_DETAILS_PATH).then().log().all().extract().response().asString();
+        String resOrderDetails = reqGetOrder.when().get(Routes.ORDER_DETAILS_PATH).then().log().all().extract().response().asString();
 
+        Map<String, String> expectedValues = dataTable.transpose().asMap();
+
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("data.orderBy"), expectedValues.get("orderBy"));
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("data.productName"), expectedValues.get("productName"));
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("data.country"), expectedValues.get("country"));
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("data.orderPrice"), expectedValues.get("orderPrice"));
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("data.productDescription"), expectedValues.get("productDescription"));
+        Assert.assertEquals(new JsonPath(resOrderDetails).getString("message"), expectedValues.get("message"));
+    }
+
+
+    @And("^I delete my order$")
+    public void delete_order() {
+
+        RequestSpecification deleteOrderReqSpec = new RequestSpecBuilder().setBaseUri(Routes.BASE_URL)
+                .addHeader("Authorization", context.getToken()).build();
+
+        RequestSpecification reqDeleteProduct =
+                given().spec(deleteOrderReqSpec).log().all()
+                        .pathParams("orderId", context.getOrderId());
+
+        String resDeleteProduct = reqDeleteProduct.when().delete(Routes.DELETE_ORDER_PATH)
+                .then().log().all().extract().response().asString();
+
+        Assert.assertEquals(new JsonPath(resDeleteProduct).getString("message"), "Orders Deleted Successfully");
+    }
+
+
+    @And("^I delete added product$")
+    public void delete_product() {
+
+        RequestSpecification deleteProductReqSpec = new RequestSpecBuilder().setBaseUri(Routes.BASE_URL)
+                .addHeader("Authorization", context.getToken()).build();
+
+        RequestSpecification reqDeleteProduct =
+                given().spec(deleteProductReqSpec).log().all()
+                        .pathParams("productId", context.getProductId());
+
+        String resDeleteProduct = reqDeleteProduct.when().delete(Routes.DELETE_PRODUCT_PATH)
+                .then().log().all().extract().response().asString();
+
+        Assert.assertEquals(new JsonPath(resDeleteProduct).getString("message"), "Product Deleted Successfully");
     }
 
 
