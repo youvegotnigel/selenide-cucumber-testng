5a286b0e230bf0477677b069ec0b377011f6fc12
Nigel <nigeldave19@gmail.com>
2025-07-06 14:35:40 +0530

    Create ApiTestContext.java

---
+++ src/test/java/org/youvegotnigel/automation/context/ApiTestContext.java
@@ -0,0 +1,42 @@
+package org.youvegotnigel.automation.context;
+
+public class ApiTestContext {
+
+    private String token;
+    private String userId;
+    private String productId;
+    private String orderId;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+}
