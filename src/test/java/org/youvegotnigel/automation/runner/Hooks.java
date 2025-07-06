831fd9a04075c93c1ce770b660d3c2d2df6e6498
Nigel <nigeldave19@gmail.com>
2025-07-06 13:02:10 +0530

    Update Hooks.java

--- src/test/java/org/youvegotnigel/automation/runner/Hooks.java
+++ src/test/java/org/youvegotnigel/automation/runner/Hooks.java
@@ -1,11 +1,17 @@
 package org.youvegotnigel.automation.runner;
 
 import com.codeborne.selenide.Configuration;
-import io.cucumber.java.AfterAll;
-import io.cucumber.java.BeforeAll;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
 import org.testng.annotations.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
+
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class Hooks {
 
@@ -18,6 +24,17 @@ public class Hooks {
 
     @AfterClass
     public static void tearDown() {
+        System.out.println("closing browser");
         closeWebDriver();
+    }
+
+    @AfterStep
+    public void take_screenshot(Scenario scenario) {
+
+        if(scenario.isFailed()) {
+            String screenshotAsBase64 = screenshot(OutputType.BASE64);
+            byte[] decoded = Base64.getDecoder().decode(screenshotAsBase64);
+            scenario.attach(decoded, "image/png", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MMM-yyyy_HH:mm:ss")));
+        }
     }
 }
