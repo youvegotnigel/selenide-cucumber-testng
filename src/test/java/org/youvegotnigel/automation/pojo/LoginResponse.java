a741866b824d234eb0f594589b0c4fa98ce7b88e
Nigel <nigeldave19@gmail.com>
2025-07-06 14:35:51 +0530

    Create LoginResponse.java

---
+++ src/test/java/org/youvegotnigel/automation/pojo/LoginResponse.java
@@ -0,0 +1,32 @@
+package org.youvegotnigel.automation.pojo;
+
+public class LoginResponse {
+
+    private String token;
+    private String userId;
+    private String message;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
