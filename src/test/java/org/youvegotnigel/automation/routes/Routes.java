898f94abaa7ca080048dae0dbcc973bb4f3a9acd
Nigel <nigeldave19@gmail.com>
2025-07-06 18:19:36 +0530

    Update Routes.java

--- src/test/java/org/youvegotnigel/automation/routes/Routes.java
+++ src/test/java/org/youvegotnigel/automation/routes/Routes.java
@@ -7,6 +7,8 @@ public class Routes {
     public static final String ADD_PRODUCT_PATH = "api/ecom/product/add-product";
     public static final String CREATE_ORDER_PATH = "api/ecom/order/create-order";
     public static final String ORDER_DETAILS_PATH = "api/ecom/order/get-orders-details";
+    public static final String DELETE_ORDER_PATH = "api/ecom/order/delete-order/{orderId}";
+    public static final String DELETE_PRODUCT_PATH = "api/ecom/product/delete-product/{productId}";
 
     public static final String WATCH_IMAGE_PATH = System.getProperty("user.dir") + "/images/watch.png";
     public static final String SHOE_IMAGE_PATH = System.getProperty("user.dir") + "/images/shoe.png";
