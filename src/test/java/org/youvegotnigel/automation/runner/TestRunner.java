aa061e548b7f886b6cc8f2c7258037cfe1c28754
Nigel <nigeldave19@gmail.com>
2025-07-06 13:02:12 +0530

    Update TestRunner.java

--- src/test/java/org/youvegotnigel/automation/runner/TestRunner.java
+++ src/test/java/org/youvegotnigel/automation/runner/TestRunner.java
@@ -1,7 +1,6 @@
 package org.youvegotnigel.automation.runner;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.*;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
