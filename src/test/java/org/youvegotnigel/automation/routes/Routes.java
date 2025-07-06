421d0bc86a13c30051ac5cb8689f480071a676dc
Nigel <nigeldave19@gmail.com>
2025-07-06 17:55:14 +0530

    Update Routes.java

--- src/test/java/org/youvegotnigel/automation/routes/Routes.java
+++ src/test/java/org/youvegotnigel/automation/routes/Routes.java
@@ -5,6 +5,8 @@ public class Routes {
     public static final String BASE_URL = "https://rahulshettyacademy.com";
     public static final String LOGIN_PATH = "api/ecom/auth/login";
     public static final String ADD_PRODUCT_PATH = "api/ecom/product/add-product";
+    public static final String CREATE_ORDER_PATH = "api/ecom/order/create-order";
+    public static final String ORDER_DETAILS_PATH = "api/ecom/order/get-orders-details";
 
     public static final String WATCH_IMAGE_PATH = System.getProperty("user.dir") + "/images/watch.png";
     public static final String SHOE_IMAGE_PATH = System.getProperty("user.dir") + "/images/shoe.png";
