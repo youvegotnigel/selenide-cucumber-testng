77acb0a24b5c9a1ed64383817b65dc1a3986c5ca
Nigel <nigeldave19@gmail.com>
2025-07-06 14:35:47 +0530

    Create Routes.java

---
+++ src/test/java/org/youvegotnigel/automation/routes/Routes.java
@@ -0,0 +1,12 @@
+package org.youvegotnigel.automation.routes;
+
+public class Routes {
+
+    public static final String BASE_URL = "https://rahulshettyacademy.com";
+    public static final String LOGIN_PATH = "api/ecom/auth/login";
+    public static final String ADD_PRODUCT_PATH = "api/ecom/product/add-product";
+
+    public static final String WATCH_IMAGE_PATH = System.getProperty("user.dir") + "/images/watch.png";
+    public static final String SHOE_IMAGE_PATH = System.getProperty("user.dir") + "/images/shoe.png";
+
+}
