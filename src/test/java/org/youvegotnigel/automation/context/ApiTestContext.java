475aee4011c7d414cb211b4bb2f2a45357466e3f
Nigel <nigeldave19@gmail.com>
2025-07-10 22:43:41 +0530

    Update ApiTestContext.java

--- src/test/java/org/youvegotnigel/automation/context/ApiTestContext.java
+++ src/test/java/org/youvegotnigel/automation/context/ApiTestContext.java
@@ -1,11 +1,32 @@
 package org.youvegotnigel.automation.context;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiTestContext {
 
+    private final Map<String, Object> context = new HashMap<>();
     private String token;
     private String userId;
     private String productId;
     private String orderId;
+
+    public void setContext(String key, Object value) {
+        context.put(key, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getContext(String key) {
+        return (T) context.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return context.containsKey(key);
+    }
+
+    public void clearContext() {
+        context.clear();
+    }
 
     public String getToken() {
         return token;
